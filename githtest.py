9192835f94fb6f05906a7c0f0c056448be4b8ff6
Daniel Jönsson <daniel.jonsson0131@gmail.com>
2024-02-06 11:03:51 +0100

    La till dokumentation

--- githtest.py
+++ githtest.py
@@ -1,6 +1,9 @@
+"""This is a test file for github"""
+
+print("Hello World")
 print("Hello Worldy")
 
-list_of_number = list[1, 2, 3, 4, 5]
+list_of_number = [1, 2, 3, 4, 5]
 
 for i in list_of_number:
     print(i)
